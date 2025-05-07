a22e6f0e8ed9130b74a9c563e4c4523b036d070d
Argi <ragiithub@gmail.com>
2025-05-07 21:04:49 +0700

    -

--- .NET Core MVC - The Complete Guide 2025 [E-commerce]/Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ .NET Core MVC - The Complete Guide 2025 [E-commerce]/Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -8,9 +8,23 @@
             return View(objCategoryList);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(category);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
--- .NET Core MVC - The Complete Guide 2025 [E-commerce]/Bulky/BulkyWeb/Models/Category.cs
+++ .NET Core MVC - The Complete Guide 2025 [E-commerce]/Bulky/BulkyWeb/Models/Category.cs
@@ -5,8 +5,12 @@
         public int Id { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
+        [Display(Name = "Category Name")]
         public string Name { get; set; } = string.Empty;
 
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
         public int DisplayOrder { get; set; }
     }
 }
