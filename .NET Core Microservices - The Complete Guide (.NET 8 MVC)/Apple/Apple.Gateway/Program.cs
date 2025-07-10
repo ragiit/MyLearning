8e72ad0afb2ac6351d2e0d02b2ee4381d90fba85
Stylus <ragiithub@gmail.com>
2025-07-10 22:29:14 +0700

    no message

---
+++ .NET Core Microservices - The Complete Guide (.NET 8 MVC)/Apple/Apple.Gateway/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var secret = builder.Configuration["ApiSettings:JwtOptions:Secret"];
+var issuer = builder.Configuration["ApiSettings:JwtOptions:Issuer"];
+var audience = builder.Configuration["ApiSettings:JwtOptions:Audience"];
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secret)),
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+            ValidateLifetime = true,
+        };
+    });
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseOcelot().GetAwaiter().GetResult();
+
+app.Run();
