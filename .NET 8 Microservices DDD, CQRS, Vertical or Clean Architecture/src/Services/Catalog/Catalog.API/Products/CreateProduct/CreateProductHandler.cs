44cfd3e7888cabb66257bdf1fe1bc3f0d2df15e2
Argi <ragiithub@gmail.com>
2025-07-11 14:58:43 +0700

    .NET 8 Microservices: DDD, CQRS, Vertical/Clean Architecture (Section 5)

---
+++ .NET 8 Microservices DDD, CQRS, Vertical or Clean Architecture/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price
+    ) : ICommand<CreateProductResult>;
+
+    public record class CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
