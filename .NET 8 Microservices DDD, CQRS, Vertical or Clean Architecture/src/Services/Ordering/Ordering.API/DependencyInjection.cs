c3d6d463976ffc4f6105736c6377c9d972206ddb
Stylus <ragiithub@gmail.com>
2025-07-16 20:39:44 +0700

    .NET 8 Microservices: DDD, CQRS, Vertical/Clean Architecture (Section 17)

--- .NET 8 Microservices DDD, CQRS, Vertical or Clean Architecture/src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ .NET 8 Microservices DDD, CQRS, Vertical or Clean Architecture/src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,31 @@
-﻿namespace Ordering.API
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddCarter();
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                       .AddSqlServer(configuration.GetConnectionString("OrderDb")!);
             return services;
         }
 
         public static WebApplication UseApplicationServices(this WebApplication application)
         {
+            application.MapCarter();
+
+            application.UseExceptionHandler(options => { });
+            application.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             return application;
         }
     }
--- .NET 8 Microservices DDD, CQRS, Vertical or Clean Architecture/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ .NET 8 Microservices DDD, CQRS, Vertical or Clean Architecture/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,19 +1,19 @@
-﻿using MassTransit;
-using Microsoft.FeatureManagement;
+﻿using Ordering.Domain.Events;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
+
 public class OrderCreatedEventHandler
-    (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
+    (ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
-    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
-
-        if (await featureManager.IsEnabledAsync("OrderFullfilment"))
-        {
-            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
-        }
+        return Task.CompletedTask;
+        //if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+        //{
+        //    var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+        //    await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        //}
     }
 }
