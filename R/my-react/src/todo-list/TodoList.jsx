612e15a25053b743d2dcd7d2af12146188de0e39
Stylus <ragiithub@gmail.com>
2025-08-18 18:38:03 +0700

    no message

--- R/my-react/src/todo-list/TodoList.jsx
+++ R/my-react/src/todo-list/TodoList.jsx
@@ -1,12 +1,18 @@
 import Todo from "./Todo";
 
 export default function TodoList() {
-  return (
-    <ul>
-      <Todo text="Learn React" isCompleted={false} isDeleted={true} />
-      <Todo text="Build a Todo App" isCompleted={true} />
-      <Todo text="Deploy the App" isCompleted={false} />
-      <Todo text="Share with Friends" isCompleted={true} />
-    </ul>
-  );
+  const data = [
+    { text: "Learn React", isCompleted: false, isDeleted: true },
+    { text: "Build a Todo App", isCompleted: true },
+    { text: "Deploy the App", isCompleted: false },
+    { text: "Share with Friends", isCompleted: true },
+  ];
+  const todos = data.map((todo, index) => <Todo key={index} {...todo} />);
+  return <ul>{todos}</ul>;
+  // <ul>
+  //   <Todo text="Learn React" isCompleted={false} isDeleted={true} />
+  //   <Todo text="Build a Todo App" isCompleted={true} />
+  //   <Todo text="Deploy the App" isCompleted={false} />
+  //   <Todo text="Share with Friends" isCompleted={true} />
+  // </ul>
 }
